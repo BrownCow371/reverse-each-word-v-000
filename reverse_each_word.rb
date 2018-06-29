b66942363c3f7eba6c9085020aa2f7595d3e960e
Jamie Brown <jmbrowncow@gmail.com>
2018-06-29 17:30:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentance)
-  new_sentance=""
   sent_array = sentance.split(" ")
-  sent_array.each{|word|
-  new_sentance<< "#{word.reverse} "
-    }
-    new_sentance.chop
+  reverse= (sent_array.map{|word|
+    word.reverse
+    })
+  reverse.join(" ")
 end
